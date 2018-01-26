c53ca53107f8b4e8cbe1c8f409735af1d1b4baa6
Chun Yang <canadachunyang@gmail.com>
2018-01-26 20:21:13 +0000

    make paino key and can play song

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => res.render('index'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
