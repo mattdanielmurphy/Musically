c9fc4ee7345a04094141da27ca2c388e967b3556
Chun Yang <canadachunyang@gmail.com>
2018-01-29 16:25:14 +0000

    full paino keyboards

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ const app = express()
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => res.render('index'))
+app.get('/compose', (req, res) => res.render('compose'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
