c1ca143a98c72d6b978d0612defc13eb1aa3214d
Matthew Daniel Murphy <matt@mattmurphy.ca>
2018-02-07 20:06:18 -0500

    fix JSX

--- react_with_backend/react-backend/client/src/App.jsx
+++ react_with_backend/react-backend/client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import NavBar from './NavBar.js';
+import NavBar from './NavBar';
 
 import './App.css'
 
